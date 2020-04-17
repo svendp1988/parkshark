12c9f8d1ba9c105250d3915667c460c5d16079b6
Sven De Potter <sven.depotter@hotmail.com>
2020-04-17 16:41:55 +0200

    initial skeleton

---
+++ parkshark/src/test/java/south/park/parkshark/ParksharkApplicationTests.java
@@ -0,0 +1,13 @@
+package south.park.parkshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParksharkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
