a364325197d1f955b7a26f773900d6738a64fa60
Harold Brinkhof <harold.brinkhof@economie.fgov.be>
2020-04-21 00:11:46 +0200

    MemberController

---
+++ api/src/main/java/south/park/parkshark/MemberController.java
@@ -0,0 +1,26 @@
+package south.park.parkshark;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import south.park.parkshark.domain.dto.request.CreateMemberDto;
+import south.park.parkshark.domain.dto.response.MemberDto;
+import south.park.parkshark.domain.service.MemberManagement;
+
+@RestController
+@RequestMapping(path = "/member", consumes = "application/json;charset=UTF-8", produces = "application/json;charset=UTF-8")
+public class MemberController {
+    private final MemberManagement memberManagement;
+
+    @Autowired
+    MemberController(MemberManagement memberManagement){
+        this.memberManagement = memberManagement;
+    }
+
+    @PostMapping
+    public MemberDto registerAsMember(@RequestBody CreateMemberDto input){
+        return memberManagement.registerMember(input);
+    }
+}
