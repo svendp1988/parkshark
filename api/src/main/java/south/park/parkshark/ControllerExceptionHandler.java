f17df04d4fb3a132572e8ee1cff265aefa799fb0
Sven De Potter <sven.depotter@hotmail.com>
2020-04-21 16:16:19 +0200

    push

--- api/src/main/java/south/park/parkshark/ControllerExceptionHandler.java
+++ api/src/main/java/south/park/parkshark/ControllerExceptionHandler.java
@@ -2,14 +2,24 @@ package south.park.parkshark;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import south.park.parkshark.domain.exceptions.InvalidEmailException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     private final static Logger LOGGER = LoggerFactory.getLogger(MemberController.class);
 
-
+    @ExceptionHandler(InvalidEmailException.class)
+    protected void invalidEmailException(InvalidEmailException ex, HttpServletResponse response) throws IOException {
+        LOGGER.error("Email is not valid!", ex);
+        response.sendError(HttpStatus.I_AM_A_TEAPOT.value(), ex.getMessage());
+    }
 
 }
