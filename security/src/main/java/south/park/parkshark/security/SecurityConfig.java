a336ed17714ab5bc171ba5351791c1ef2a2a8541
Harold Brinkhof <harold.brinkhof@economie.fgov.be>
2020-04-23 13:39:23 +0200

    fixed security to allow anonymous POST access to /member

--- security/src/main/java/south/park/parkshark/security/SecurityConfig.java
+++ security/src/main/java/south/park/parkshark/security/SecurityConfig.java
@@ -4,9 +4,11 @@ package south.park.parkshark.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -28,9 +30,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers( HttpMethod.POST, "/member");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/member").permitAll()
+                .and().authorizeRequests()
                 .antMatchers("/**")
                 .permitAll()
                 .anyRequest().authenticated()
